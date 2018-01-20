283fad052696c638e11c142feaefb247cb33c024
Roman Pearah <roman.pearah@tranzact.net>
2018-01-19 21:56:36 -0500

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-watermark "1.0.0"
+(defproject onyx-watermark "1.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
